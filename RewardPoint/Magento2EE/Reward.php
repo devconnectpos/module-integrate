30e3554f730c7e336b0c3800d0d6f56b57d54b7f
xuantung <tungdx@smartosc.com>
2020-07-30 17:51:48 +0700

    Update version 20.07.30

--- RewardPoint/Magento2EE/Reward.php
+++ RewardPoint/Magento2EE/Reward.php
@@ -14,6 +14,25 @@ if (class_exists("\Magento\Reward\Model\Reward")) {
 					self::REWARD_ACTION_REFUND_WITHOUT_RECEIPT => \SM\Integrate\RewardPoint\Magento2EE\Action\RefundWithoutReceipt::class,
 				];
 		}
+		
+		/**
+		 * Return rate direction by action
+		 *
+		 * @return integer
+		 */
+		public function getRateDirectionByAction()
+		{
+			switch ($this->getAction()) {
+				case self::REWARD_ACTION_ORDER_EXTRA:
+				case self::REWARD_ACTION_REFUND_WITHOUT_RECEIPT:
+					$direction = \Magento\Reward\Model\Reward\Rate::RATE_EXCHANGE_DIRECTION_TO_POINTS;
+					break;
+				default:
+					$direction = \Magento\Reward\Model\Reward\Rate::RATE_EXCHANGE_DIRECTION_TO_CURRENCY;
+					break;
+			}
+			return $direction;
+		}
 	}
 } else {
 	class Reward
